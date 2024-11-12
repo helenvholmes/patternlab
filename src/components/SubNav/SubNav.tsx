7c805415afc50c98a5e0e3f7d0a653680ae30c66
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2024-11-12 13:00:46 -0500

    removed selected item

--- src/components/SubNav/SubNav.tsx
+++ src/components/SubNav/SubNav.tsx
@@ -181,131 +181,139 @@ export const SubNav: ChakraComponent<
   >,
   SubNavProps
 > = chakra(
-  forwardRef<HTMLDivElement, React.PropsWithChildren<SubNavProps>>((props, _ref?) => {
-    const {
-      className,
-      actionBackgroundColor,
-      highlightColor,
-      primaryActions,
-      secondaryActions,
-    } = props;
+  forwardRef<HTMLDivElement, React.PropsWithChildren<SubNavProps>>(
+    (props, _ref?) => {
+      const {
+        className,
+        actionBackgroundColor,
+        highlightColor,
+        primaryActions,
+        secondaryActions,
+      } = props;
 
-    const [showRightFade, setShowRightFade] = useState(false);
-    const [lastScrollLeft, setLastScrollLeft] = useState(0);
-    const scrollableRef = useRef(null);
+      const [showRightFade, setShowRightFade] = useState(false);
+      const [lastScrollLeft, setLastScrollLeft] = useState(0);
+      const scrollableRef = useRef(null);
 
-    if (actionBackgroundColor !== undefined && highlightColor === undefined) {
-      console.warn(
-        "NYPL Reservoir SubNav: The `actionBackgroundColor` prop has been passed, but the `highlightColor` prop has not been passed. Because of this, the `actionBackgroundColor` prop will be ignored."
-      );
-    }
-
-    const validateActions = (actions: React.ReactNode, propName: string) => {
-      if (React.isValidElement(actions)) {
-        React.Children.forEach(actions.props.children, (child: React.ReactElement) => {
-          if (child.type !== SubNavButton && child.type !== SubNavLink) {
-            console.warn(`NYPL Reservoir SubNav: An element that is not a SubNavButton or SubNavLink component has been passed in the \`${propName}\` prop. That element will be ignored.`);
-            return null;
-          }
-        });
-      }
-    };
-
-    // Validate primaryActions and secondaryActions
-    validateActions(primaryActions, 'primaryActions');
-    validateActions(secondaryActions, 'secondaryActions');
-
-    const backgroundColor =
-      highlightColor !== undefined ? actionBackgroundColor : undefined;
-
-    const styles = useMultiStyleConfig("SubNav", {
-      backgroundColor: backgroundColor,
-      highlightColor: highlightColor,
-    });
-
-    const handleScroll = () => {
-      if (scrollableRef.current) {
-        const { scrollLeft, scrollWidth, clientWidth } = scrollableRef.current;
-
-        // Determine if we are scrolling to the left or right
-        const isScrollingRight = scrollLeft > lastScrollLeft;
-        setLastScrollLeft(scrollLeft); // Update last scroll position
-
-        // Show right fade only if scrolling to the left
-        if (!isScrollingRight) {
-          // Show right fade only if not at the end
-          const atRightEdge = scrollLeft + clientWidth >= scrollWidth;
-          setShowRightFade(!atRightEdge);
-        } else {
-          setShowRightFade(false); // Hide when scrolling right
-        }
-      }
-    };
-
-    useEffect(() => {
-      const refCurrent = scrollableRef.current;
-      if (refCurrent) {
-        refCurrent.addEventListener("scroll", handleScroll);
-
-        // Initial check to set the right fade effect based on the initial scroll state
-        const { scrollWidth, clientWidth, scrollLeft } = refCurrent;
-        const atRightEdge = scrollLeft + clientWidth >= scrollWidth;
-
-        // Set the right fade effect if content is scrollable and not at the right edge
-        setShowRightFade(!atRightEdge);
-
-        // If the scrollable area is already at the end, hide the right fade initially
-        if (scrollWidth <= clientWidth) {
-          setShowRightFade(false); // If the content fits in the viewport, no need for a fade
-        }
+      if (actionBackgroundColor !== undefined && highlightColor === undefined) {
+        console.warn(
+          "NYPL Reservoir SubNav: The `actionBackgroundColor` prop has been passed, but the `highlightColor` prop has not been passed. Because of this, the `actionBackgroundColor` prop will be ignored."
+        );
       }
 
-      // Cleanup event listener on component unmount
-      return () => {
-        if (refCurrent) {
-          refCurrent.removeEventListener("scroll", handleScroll);
+      const validateActions = (actions: React.ReactNode, propName: string) => {
+        if (React.isValidElement(actions)) {
+          React.Children.forEach(
+            actions.props.children,
+            (child: React.ReactElement) => {
+              if (child.type !== SubNavButton && child.type !== SubNavLink) {
+                console.warn(
+                  `NYPL Reservoir SubNav: An element that is not a SubNavButton or SubNavLink component has been passed in the \`${propName}\` prop. That element will be ignored.`
+                );
+                return null;
+              }
+            }
+          );
         }
       };
-    }, []); // Empty dependency array to run only once after the initial render
 
-    return (
-      <Box __css={styles.base}>
-        <Flex
-          alignItems="baseline"
-          className={className}
-          gap="1rem"
-          justify="space-between"
-        >
-          <List
-            type="ul"
-            m="0"
-            sx={{ ...styles.scrollableButtons, ...styles.primaryActions }}
-            ref={scrollableRef}
-            inline
-            noStyling
+      // Validate primaryActions and secondaryActions
+      validateActions(primaryActions, "primaryActions");
+      validateActions(secondaryActions, "secondaryActions");
+
+      const backgroundColor =
+        highlightColor !== undefined ? actionBackgroundColor : undefined;
+
+      const styles = useMultiStyleConfig("SubNav", {
+        backgroundColor: backgroundColor,
+        highlightColor: highlightColor,
+      });
+
+      const handleScroll = () => {
+        if (scrollableRef.current) {
+          const { scrollLeft, scrollWidth, clientWidth } =
+            scrollableRef.current;
+
+          // Determine if we are scrolling to the left or right
+          const isScrollingRight = scrollLeft > lastScrollLeft;
+          setLastScrollLeft(scrollLeft); // Update last scroll position
+
+          // Show right fade only if scrolling to the left
+          if (!isScrollingRight) {
+            // Show right fade only if not at the end
+            const atRightEdge = scrollLeft + clientWidth >= scrollWidth;
+            setShowRightFade(!atRightEdge);
+          } else {
+            setShowRightFade(false); // Hide when scrolling right
+          }
+        }
+      };
+
+      useEffect(() => {
+        const refCurrent = scrollableRef.current;
+        if (refCurrent) {
+          refCurrent.addEventListener("scroll", handleScroll);
+
+          // Initial check to set the right fade effect based on the initial scroll state
+          const { scrollWidth, clientWidth, scrollLeft } = refCurrent;
+          const atRightEdge = scrollLeft + clientWidth >= scrollWidth;
+
+          // Set the right fade effect if content is scrollable and not at the right edge
+          setShowRightFade(!atRightEdge);
+
+          // If the scrollable area is already at the end, hide the right fade initially
+          if (scrollWidth <= clientWidth) {
+            setShowRightFade(false); // If the content fits in the viewport, no need for a fade
+          }
+        }
+
+        // Cleanup event listener on component unmount
+        return () => {
+          if (refCurrent) {
+            refCurrent.removeEventListener("scroll", handleScroll);
+          }
+        };
+      }, []); // Empty dependency array to run only once after the initial render
+
+      return (
+        <Box __css={styles.base}>
+          <Flex
+            alignItems="baseline"
+            className={className}
+            gap="1rem"
+            justify="space-between"
           >
-            <li id="primary-actions">{primaryActions}</li>
-            {showRightFade && (
-              // Explicitly cast styles.fadeEffect to CSSProperties
-              <div style={styles.fadeEffect as CSSProperties} />
-            )}
-          </List>
-          <List
-            noStyling
-            inline
-            type="ul"
-            sx={{ ...styles.secondaryActions }}
-            m="0"
-            width="fit-content"
-          >
-            <li id="secondary-actions">
-              {secondaryActions ? secondaryActions : null}
-            </li>
-          </List>
-        </Flex>
-      </Box>
-    );
-  })
+            <List
+              type="ul"
+              m="0"
+              sx={{ ...styles.scrollableButtons, ...styles.primaryActions }}
+              ref={scrollableRef}
+              inline
+              noStyling
+            >
+              <li id="primary-actions">{primaryActions}</li>
+              {showRightFade && (
+                // Explicitly cast styles.fadeEffect to CSSProperties
+                <div style={styles.fadeEffect as CSSProperties} />
+              )}
+            </List>
+            <List
+              noStyling
+              inline
+              type="ul"
+              sx={{ ...styles.secondaryActions }}
+              m="0"
+              width="fit-content"
+            >
+              <li id="secondary-actions">
+                {secondaryActions ? secondaryActions : null}
+              </li>
+            </List>
+          </Flex>
+        </Box>
+      );
+    }
+  )
 );
 
 export default SubNav;
