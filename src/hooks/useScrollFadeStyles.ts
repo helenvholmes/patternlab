dacf90915e7e72f35f6cf42a79a778292c972810
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2024-11-14 16:21:09 -0500

    added documentation for fading hook

--- src/hooks/useScrollFadeStyles.ts
+++ src/hooks/useScrollFadeStyles.ts
@@ -1,5 +1,11 @@
 import React, { useEffect, useState, useRef } from "react";
 
+/**
+ * The `useScrollFadeStyles` hook manages the visibility of a right-side fade effect
+ * based on horizontal scrolling. It tracks scroll events and shows the fade effect 
+ * when scrolling to the left and more content is available to the right, hiding it 
+ * when scrolling right or when at the right edge of the scrollable container.
+ */
 export default function useScrollFadeStyles() {
   const [showRightFade, setShowRightFade] = useState(false);
   const [lastScrollLeft, setLastScrollLeft] = useState(0);
