46420ce29aff5f40565c751c12970cba0b34c3cf
bigfishdesign13 <marty@brutvan.com>
2025-01-17 13:33:40 -0500

    structural adjustments

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Text, { textSizesArray } from "./Text";
 import { argsBooleanType } from "../../helpers/storybookUtils";
+import formatNumber from "../../hooks/useFormatNumber";
 
 const meta: Meta<typeof Text> = {
   title: "Components/Typography & Styles/Text",
@@ -24,6 +25,12 @@ const meta: Meta<typeof Text> = {
     },
   },
 };
+
+const copiesSold = formatNumber(81450000);
+const suggestedPrice = formatNumber(100, 20000);
+// const getFormattedNumbers = () => {
+//   formatNumber(81450000);
+// };
 
 export default meta;
 type Story = StoryObj<typeof Text>;
@@ -72,6 +79,10 @@ export const WithControls: Story = {
         games have also been released: Animal Crossing: Happy Home Designer for
         Nintendo 3DS, Animal Crossing: Amiibo Festival for Wii U, and Animal
         Crossing: Pocket Camp for mobile devices.
+      </Text>
+      <Text {...args}>
+        As of June 2024, the Animal Crossing franchise has sold over $
+        {copiesSold} copies worldwide. Suggested retail price: ${suggestedPrice}
       </Text>
     </>
   ),
